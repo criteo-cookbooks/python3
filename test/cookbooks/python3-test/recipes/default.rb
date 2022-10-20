53fde4be91c00fe182dc3ddcab3c4c541a4c39f7
Arnaud Chong <a.chong@criteo.com>
2022-10-20 20:37:36 +0200

    Update the test for the multi versions setup
    * Update the test cookbook to be able to test with different versions of
      Python
    * Create a test only on CentOS 8 with python36 and python39
    
    Change-Id: I4b8404b1215e8a6a4610f2eefd628c66d85f4a3b

--- test/cookbooks/python3-test/recipes/default.rb
+++ test/cookbooks/python3-test/recipes/default.rb
@@ -4,16 +4,28 @@
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
 
-python_install node['python3']['name']
+node['python3_test']['pythons'].each_with_index do |python, idx|
 
-python_package 'flask' do
-  version '2.0.1'
-end
+  python_install python['name'] do
+    pip_binary_name python['pip_binary_name']
+    binary_name python['binary_name']
+    version python['version']
+  end
 
-python_virtualenv '/opt/blah' do
-  action :create
-end
+  python_package 'flask' do
+    version '2.0.1'
+    pip_binary_name python['pip_binary_name']
+  end
 
-python_package 'flask' do
-  virtualenv '/opt/blah'
+  python_virtualenv "/opt/blah-#{idx}" do
+    action :create
+    pip_binary_name python['pip_binary_name']
+    binary_name python['binary_name']
+  end
+
+  python_package 'flask' do
+    virtualenv "/opt/blah-#{idx}"
+    pip_binary_name python['pip_binary_name']
+  end
+
 end
